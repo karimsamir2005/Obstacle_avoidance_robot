b0445c62e5e7f54971187f8267fb89cef1ab6927
karimsamir2005 <146492778+karimsamir2005@users.noreply.github.com>
2026-09-14 21:23:31 +0300

    initial packages of the robot

---
+++ src/robot_bringup_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_bringup_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kareem',
+    maintainer_email='146492778+karimsamir2005@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
