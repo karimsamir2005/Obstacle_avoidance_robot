aeb8692e8d846ac42739d7ace0c96c74be607f2f
karimsamir2005 <146492778+karimsamir2005@users.noreply.github.com>
2026-09-17 13:01:35 +0300

    launch file and bridge yaml configuration

--- src/robot_bringup_pkg/launch/robot_launch.py
+++ src/robot_bringup_pkg/launch/robot_launch.py
@@ -2,7 +2,7 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import command, PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -11,3 +11,104 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     #The files Paths to be passsed to the launch file
+
+    robot_urdf_file = PathJoinSubstitution([
+        FindPackageShare('robot_description_pkg'),
+        'urdf',
+        'obstacle.urdf.xacro' #the assembler xacro file that has all descriptions
+    ])
+
+    #rviz configuration file to not start from scratch everytime rviz opens
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare('robot_description_pkg'),
+        'rviz',
+        'obstacle_config.rviz'
+    ])
+
+    #gazebo/ros bridge
+    bridge_config_file = PathJoinSubstitution([
+        FindPackageShare('robot_bringup_pkg'),
+        'config',
+        'gazebo_bridge.yaml'
+    ])
+
+    #this will take the xacro file and transform it to urdf format
+    robot_description = ParameterValue (
+        Command(['xacro ' , robot_urdf_file]),
+        value_type = str
+    )
+
+    #launching gazbeo harmonic
+    start_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'gz_args': 'empty.sdf -r'
+        }.items()
+    )
+
+
+    start_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': True
+        }],
+        output='screen'
+    )
+
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', '/robot_description',
+            '-name', 'edges_robot',
+            '-x', '0',
+            '-y', '0',
+            '-z', '0.1' #slightly above the ground to ensur clean start
+        ],
+        output='screen'
+    )
+
+    
+    start_gazebo_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': bridge_config_file
+        }],
+        output='screen'
+    )
+
+    start_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config_file],
+        parameters=[{
+            'use_sim_time': True
+        }],
+        output='screen'
+    )
+
+    #the teleop keyboard to test the simulation topics and nodes
+    start_teleop = Node(
+    package='teleop_twist_keyboard',
+    executable='teleop_twist_keyboard',
+    prefix='gnome-terminal --',
+    output='screen'
+    )
+
+    return LaunchDescription([
+        start_gazebo,
+        start_robot_state_publisher,
+        spawn_robot,
+        start_gazebo_bridge,
+        start_rviz,
+        start_teleop
+    ])
--- src/robot_bringup_pkg/setup.py
+++ src/robot_bringup_pkg/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'robot_bringup_pkg'
@@ -10,18 +12,23 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        # Install all launch files:
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*')),
+        
+        # Install bridge and yaml configs:
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        
+        # Install rviz configs:
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='kareem',
-    maintainer_email='146492778+karimsamir2005@users.noreply.github.com',
-    description='TODO: Package description',
+    maintainer_email='kareem@todo.todo',
+    description='Bringup package for obstacle robot simulation',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
